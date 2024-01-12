e2c2d947c738e1ee27467df81ebca373ffd1c461
Francis <fhrancorey99@gmail.com>
2024-01-12 07:08:56 +0100

    deployment updates

--- library/django_project/wsgi.py
+++ library/django_project/wsgi.py
@@ -11,4 +11,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
